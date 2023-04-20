1f77c995dd55bd520408a428b3155f8083082486
Phap Huynh Van <phaphv@nal.vn>
2023-04-21 00:46:10 +0700

    validate doctor's input data and doctor's update data

--- resources/views/admin/doctors/edit.blade.php
+++ resources/views/admin/doctors/edit.blade.php
@@ -31,7 +31,7 @@
                         <label for="exampleInputName1">Doctor's Name</label>
                         <input
                             type="text"
-                            class="form-control"
+                            class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
                             id="exampleInputName1"
                             placeholder="Name"
                             name="name"
@@ -39,39 +39,54 @@
                         >
                     </div>
 
+                    @if ($errors->has('name'))
+                        <span id="exampleInputPassword1-error" class="error invalid-feedback">{{ $errors->first('name') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group">
-                    <input type="file" name="image" class="file-upload-default">
+                    <input type="file" name="image" class="file-upload-default {{ $errors->has('image') ? 'is-invalid' : '' }}">
                     <div class="input-group col-xs-12">
-                        <input type="text" class="form-control file-upload-info" disabled="" placeholder="Doctor's Image">
+                        <input type="text" class="form-control file-upload-info {{ $errors->has('image') ? 'is-invalid' : '' }}" disabled="" placeholder="Doctor's Image">
                         <span class="input-group-append">
                             <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
                         </span>
                     </div>
+
+                    @if ($errors->has('image'))
+                        <span id="exampleInputPassword1-error" class="error invalid-feedback">{{ $errors->first('image') }}</span>
+                    @endif
                 </div>
 
                 <div class="form-group">
                     <label for="exampleInputName1">Room No</label>
                     <input
                         type="text"
-                        class="form-control"
+                        class="form-control {{ $errors->has('room_id') ? 'is-invalid' : '' }}"
                         id="exampleInputName1"
                         placeholder="Input the room number"
                         name="room_id"
                         value="{{ $doctor->room_id }}"
                     >
+
+                    @if ($errors->has('room_id'))
+                        <span id="exampleInputPassword1-error" class="error invalid-feedback">{{ $errors->first('room_id') }}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="exampleInputEmail3">Phone</label>
                     <input
                         type="number"
-                        class="form-control"
+                        class="form-control {{ $errors->has('phone') ? 'is-invalid' : '' }}"
                         id="exampleInputEmail3"
                         placeholder="Phone"
                         name="phone"
                         value="{{ $doctor->phone }}"
                     >
+
+                    @if ($errors->has('phone'))
+                        <span id="exampleInputPassword1-error" class="error invalid-feedback">{{ $errors->first('phone') }}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="exampleSelectGender">Speciality</label>
@@ -79,7 +94,7 @@
                         'name' => 'speciality',
                         'id' => 'exampleSelectGender',
                         'data' => config('constraint.speciality'),
-                        'defaultValue' => $doctor->speciality
+                        'defaultValue' => config('constraint.speciality.'.$doctor->speciality)
                     ])
                 </div>
 
